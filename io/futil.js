9db15f70a606c2a9410e6fdb53efcec759f02085
Ghasem Kiani <ghasemkiani@gmail.com>
2019-02-12 00:22:21 +0330

    Fixed errors in io/futil

--- io/futil.js
+++ io/futil.js
@@ -10,28 +10,30 @@ class FUtil extends Base {
 	async toCopyDir(dir1, dir2) {
 		if(fs.existsSync(dir1)) {
 			await fsPromises.mkdir(dir2, {recursive: true});
-			for(let f of await fsPromises.readdir(dir1, {withFileTypes: true})) {
+			let ff = await fsPromises.readdir(dir1, {withFileTypes: true});
+			for(let f of ff) {
 				let p1 = path.join(dir1, f.name);
 				let p2 = path.join(dir2, f.name);
 				if(f.isFile()) {
 					await fsPromises.copyFile(p1, p2);
 				} else if(f.isDirectory()) {
-					return await this.toCopyDir(p1, p2);
+					await this.toCopyDir(p1, p2);
 				}
 			}
 		}
 	}
 	async toDelDir(dir) {
 		if(fs.existsSync(dir)) {
-			for(let f of await fsPromises.readdir(dir, {withFileTypes: true})) {
+			let ff = fs.readdirSync(dir, {withFileTypes: true});
+			for(let f of ff) {
 				let p = path.join(dir, f.name);
 				if(f.isFile()) {
-					await fsPromises.unlink(p);
+					fs.unlinkSync(p);
 				} else if(f.isDirectory()) {
-					return await this.toDelDir(p);
+					await this.toDelDir(p);
 				}
 			}
-			await fsPromises.rmdir(dir);
+			fs.rmdirSync(dir);
 		}
 	}
 }
