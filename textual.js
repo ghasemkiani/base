d7629e8c33fec6086beace4ea8aa30c28f06afbc
Ghasem Kiani <ghasemkiani@gmail.com>
2021-07-19 10:50:44 +0430

    update

--- textual.js
+++ textual.js
@@ -5,7 +5,12 @@ const {Obj: Base} = require("@ghasemkiani/base/obj");
 const {serializable} = require("@ghasemkiani/base/serializable");
 
 class Textual extends cutil.mixin(Base, serializable) {
-	//
+	get json() {
+		return JSON.parse(this.string);
+	}
+	set json(json) {
+		this.string = JSON.stringify(json);
+	}
 }
 cutil.extend(Textual.prototype, {
 	//
